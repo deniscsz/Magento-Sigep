4e33a45f9054191b358c0bfb32ec5edf2cef2998
Denis Colli Spalenza <deniscsz@gmail.com>
2014-08-29 02:29:34 -0300

    Updating repository
    Sending changes that were made a year ago, now works.

--- app/code/community/Xpd/Sigep/controllers/Adminhtml/Sales/OrderController.php
+++ app/code/community/Xpd/Sigep/controllers/Adminhtml/Sales/OrderController.php
@@ -18,24 +18,28 @@ class Xpd_Sigep_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Sales_Ord
         $file = fopen($_file,"a");
         
         if($file)
-        {   
-            fwrite($file,"Número;CNPJ/CPF;Nome;EMAIL;Cep;Logradouro;Número;Complemento;Bairro;Cidade;Telefone;Celular\r\n");
+        {
+            fwrite($file,"1SIGEP DESTINATARIO NACIONAL\n");
+            //fwrite($file,"Número;CNPJ/CPF;Nome;EMAIL;Cep;Logradouro;Número;Complemento;Bairro;Cidade;Telefone;Celular\r\n");
             $shippings = $sigep->getShipping2Csv($_file,$orderIds);
             
             foreach ($shippings as $_incrementId => $_fields)
             {
-                $write = fwrite($file,"2;".
-                    $_fields['cpf'].";".
-                    $_fields['nome'].";".
-                    $_fields['email'].";".
-                    $_fields['cep'].";".
-                    $_fields['logradouro'].";".
-                    $_fields['numero'].";".
-                    $_fields['complemento'].";".
-                    $_fields['bairro'].";".
-                    $_fields['cidade'].";".
-                    $_fields['telefone'].";".
-                    $_fields['celular']."\r\n"
+                $write = fwrite($file,"2".
+                    str_pad($_fields['cpf'], 14, " ", STR_PAD_RIGHT).
+                    str_pad($this->retira_acentos($_fields['nome']), 50, " ", STR_PAD_RIGHT).
+                    str_pad($_fields['email'], 50, " ", STR_PAD_RIGHT).
+                    str_pad('', 50, " ", STR_PAD_RIGHT).
+                    str_pad('', 50, " ", STR_PAD_RIGHT).
+                    str_pad($_fields['cep'], 8, " ", STR_PAD_RIGHT).
+                    str_pad($this->retira_acentos($_fields['logradouro']), 50, " ", STR_PAD_RIGHT).
+                    str_pad($_fields['numero'], 6, " ", STR_PAD_RIGHT).
+                    str_pad($this->retira_acentos($_fields['complemento']), 30, " ", STR_PAD_RIGHT).
+                    str_pad($this->retira_acentos($_fields['bairro']), 50, " ", STR_PAD_RIGHT).
+                    str_pad($this->retira_acentos($_fields['cidade']), 50, " ", STR_PAD_RIGHT).
+                    str_pad($_fields['telefone'], 18, " ", STR_PAD_RIGHT).
+                    str_pad($_fields['celular'], 12, " ", STR_PAD_RIGHT).
+                    str_pad('', 12, " ", STR_PAD_RIGHT)."\n"
                 );
                 
                 if($write)
@@ -47,6 +51,8 @@ class Xpd_Sigep_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Sales_Ord
                     Mage::log('CSV NÃO FOI ATUALIZADO');
                 }
             }
+            
+            fwrite($file,"9".str_pad(count($shippings), 6, "0", STR_PAD_LEFT)."\n");
         }
         else
         {
@@ -72,4 +78,12 @@ class Xpd_Sigep_Adminhtml_Sales_OrderController extends Mage_Adminhtml_Sales_Ord
         
         $this->_redirect('*/*/');	
     }
+	
+	public function retira_acentos($texto){
+	    $array1 = array( "á", "à", "â", "ã", "ä", "é", "è", "ê", "ë", "í", "ì", "î", "ï", "ó", "ò", "ô", "õ", "ö", "ú", "ù", "û", "ü", "ç" 
+, "Á", "À", "Â", "Ã", "Ä", "É", "È", "Ê", "Ë", "Í", "Ì", "Î", "Ï", "Ó", "Ò", "Ô", "Õ", "Ö", "Ú", "Ù", "Û", "Ü", "Ç" ); 
+		$array2 = array( "a", "a", "a", "a", "a", "e", "e", "e", "e", "i", "i", "i", "i", "o", "o", "o", "o", "o", "u", "u", "u", "u", "c" 
+		, "A", "A", "A", "A", "A", "E", "E", "E", "E", "I", "I", "I", "I", "O", "O", "O", "O", "O", "U", "U", "U", "U", "C" ); 
+		return str_replace( $array1, $array2, $texto); 
+	}
 }
